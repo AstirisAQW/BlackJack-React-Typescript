c1923c0b0f2ed13b211eed5040e1ee07971206ed
AstirisAQW <deysolong.josephjose457@gmail.com>
2025-06-30 20:26:30 +0800

    Made Dealer Logic

---
+++ src/domain/usecase/DealerLogic.ts
@@ -0,0 +1,23 @@
+import { Card } from "../entity/CardEntity";
+import { CalculateScore } from "./CalculateScore";
+
+export function DealerLogic(DealerHand: Card[], deck: Card[]): { FinalHand: Card[], UpdatedDeck: Card[] }{
+    let CurrentHand = [...DealerHand];
+    let CurrentDeck = [...deck];
+    let DealerScore = CalculateScore(CurrentHand);
+
+    while (DealerScore < 17) {
+        const card = CurrentDeck.pop();
+        if (card) {
+            CurrentHand.push(card);
+            DealerScore = CalculateScore(CurrentHand);
+        } else {
+            break;
+        }
+    }
+
+    return {
+        FinalHand: CurrentHand,
+        UpdatedDeck: CurrentDeck,
+    };
+}
