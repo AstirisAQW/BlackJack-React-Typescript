fa7e41b0271108e463f0dbc0af9280a36c8ab9d3
AstirisAQW <deysolong.josephjose457@gmail.com>
2025-06-30 08:13:57 +0800

    calculatescore usecase

---
+++ src/domain/usecase/CalculateScore.ts
@@ -0,0 +1,29 @@
+import { Card } from "../entity/CardEntity";
+
+export function CalculateScore(hand: Card[]): number{
+    let TotalScore = 0;
+
+    hand.forEach((CardInHand) => {
+        if(CardInHand.isCardHidden) return;
+
+        if (CardInHand.CardValue === 'A'){
+            Aces.push(CardInHand)
+        } else if (['K', 'Q', 'J'].includes(CardInHand.CardValue)){
+            TotalScore += 10;
+        } else {
+            TotalScore += Number(CardInHand.CardValue);
+        }
+    });
+
+    const Aces: Card[] = [];
+
+    Aces.forEach(() =>{
+        if (TotalScore + 11 > 21){
+            TotalScore += 1;
+        } else {
+            TotalScore += 11;
+        }
+    });
+
+    return TotalScore;
+}
