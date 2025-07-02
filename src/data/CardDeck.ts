0e393bb90199b4a0e654c97b1db90332a66255d2
AstirisAQW <deysolong.josephjose457@gmail.com>
2025-07-02 08:06:18 +0800

    renamed to CardDeck

--- src/data/CardDeck.ts
+++ src/data/CardDeck.ts
@@ -1,8 +1,8 @@
-import { Card } from '../domain/entity/CardEntity';
+import { Card } from '../domain/entity/Card';
 import DeckData from './CardDeck.json';
 
 // Fisher-Yates shuffle algorithm
-const shuffle = (deck: Card[]): Card[] => {
+const ShuffleCardDeck = (deck: Card[]): Card[] => {
   for (let i = deck.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [deck[i], deck[j]] = [deck[j], deck[i]];
@@ -10,12 +10,12 @@ const shuffle = (deck: Card[]): Card[] => {
   return deck;
 };
 
-export const DeckRepository = {
-  getNewDeck: (): Card[] => {
-    const freshDeck: Card[] = (DeckData.cards as Card[]).map(card => ({
+export const CardDeck = {
+  getShuffledCardDeck: (): Card[] => {
+    const CardDeck: Card[] = (DeckData.cards as Card[]).map(card => ({
       ...card,
       hidden: false,
     }));
-    return shuffle(freshDeck);
+    return ShuffleCardDeck(CardDeck);
   },
 };
